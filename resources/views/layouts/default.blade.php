6d1fe5b6c24d19e343fab3a08a3d4876d0eaf93e
dong.chen <dong.chen@niustock.com>
2017-12-18 10:00:35 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,17 @@
 <html>
 <head>
     <title>@yield('title', 'Sample') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <header class="navbar navbar-fixed-top navbar-inverse">
+        @include('layouts._header')
+    </header>
+
+    <div class="container-fluid">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+
 </body>
 </html>
